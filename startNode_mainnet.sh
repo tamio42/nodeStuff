17bc10ba910aec071fb8b6593c77c2c0ad3a4b9f
tamioTech <62820237+tamioTech@users.noreply.github.com>
2023-04-06 10:47:20 -0700

    starnode

--- startNode_mainnet.sh
+++ startNode_mainnet.sh
@@ -2,6 +2,6 @@
    --topology $HOME/cardano-src/relays/mainnet/topology.json \
    --database-path $HOME/cardano-src/relays/mainnet/db \
    --socket-path $HOME/cardano-src/relays/mainnet/db/node.socket \
-   --host-addr 10.0.0.11 \
+   --host-addr 10.0.0.5 \
    --port 3001 \
    --config $HOME/cardano-src/relays/mainnet/config.json
--- startNode_testnet.sh
+++ startNode_testnet.sh
@@ -2,6 +2,6 @@
    --topology $HOME/cardano-src/relays/testnet/topology.json \
    --database-path $HOME/cardano-src/relays/testnet/db \
    --socket-path $HOME/cardano-src/relays/testnet/db/node.socket \
-   --host-addr 10.0.0.11 \
+   --host-addr 10.0.0.5 \
    --port 30002 \
    --config $HOME/cardano-src/relays/testnet/config.json
